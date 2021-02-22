4d55e6d3987cc5f450b6050548dc85e91a79b142
Sebastian Berns <s.berns@qmul.ac.uk>
2021-02-22 13:13:49 +0000

    Rework class methods

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ import clip
 from biggan import BigGAN
 
 
-# Download and save pre-trained BigGAN files to this directory
-os.environ['PYTORCH_PRETRAINED_BIGGAN_CACHE'] = './models/'
-
-
 def main():
     # Script arguments
     parser = argparse.ArgumentParser()
@@ -83,97 +79,6 @@ def get_device(device='cuda'):
     if not torch.cuda.is_available():
         device = 'cpu'
     return torch.device(device)
-
-
-def data_augmentation(images, num_cuts=32):
-    """ Increase the number of images by cutting out random subimages """
-    _, _, width, height = images.size()  # Get image dimensions
-    assert width == height, "Images must be squares"
-
-    cuts = []
-    for _ in range(num_cuts):
-        # Determine a smaller size to cut images out of the generated samples
-        # 1. Draw one sample from a Gaussian (centered at 0.8 with std dev 0.3)
-        # 2. Limit the value to the range [0.5, 0.95]
-        # 3. Multiply original size with smaller factor
-        # 4. Convert to integer
-        size = int(width * torch.zeros(1,).normal_(mean=.8, std=.3).clip(.5, .95))
-
-        # Get random offsets from the edge of the image
-        offsetx = torch.randint(0, width - size, ())
-        offsety = torch.randint(0, width - size, ())
-
-        # Cut out images from generated samples
-        # Dimensions are [Batch, Channels, Width, Height]
-        # Here only width and height are touched
-        cut = images[:, :, offsetx:offsetx + size, offsety:offsety + size]
-
-        # Scale random cuts to CLIP input size
-        cut = torch.nn.functional.interpolate(cut, (224, 224), mode='nearest')
-
-        cuts.append(cut)  # Collect cuts in list
-    return torch.cat(cuts, dim=0)  # Create tensor from list
-
-
-def compute_latent_loss(latvecs, threshold=1):
-    """ Latent vector loss
-        Force distribution of values in latent vectors to be as close as
-        possible to a standard Gaussian
-    """
-    threshold = torch.tensor(threshold, device=latvecs.device)
-
-    # First and second standardized moments (mean and standard deviation)
-    mean = latvecs.mean().abs()  # Absolute mean of latents
-    # Mean of absolute difference of lat vec’s stdev to 1 (normal Gaussian)
-    std = torch.abs(1 - latvecs.std(dim=1)).mean()
-    max = torch.max(latvecs.square().mean(),  # Mean of squared latent vecs
-                    threshold)  # Latent threshold (default: 1)
-
-    lat_loss = mean + std + 4 * max
-
-    # Score normalization for calculation of skewness and kurtosis
-    # In batch but for each latent vector (each row) separately
-    vec_means = latvecs.mean(dim=1, keepdim=True)  # Compute means
-    vec_diffs = latvecs - vec_means  # Compute deviations from means
-    vec_vars = vec_diffs.square().mean(dim=1, keepdim=True)  # Variances
-    vec_stds = vec_vars.sqrt()  # Standard deviations
-    vec_scores = vec_diffs / vec_stds  # Normalized scores
-
-    # Third and fourth standardized moments
-    # Compute skewness and kurtosis of values in latent vector
-    skewness = vec_scores.pow(3.0).mean(dim=1)
-    kurtosis = vec_scores.pow(4.0).mean(dim=1) - 3.0
-
-    # Absolute deviations from normal Gaussian skewness (0) and kurtosis (3)
-    # Compute average over all vectors (rows)
-    skewness = skewness.abs().mean()
-    kurtosis = kurtosis.abs().mean()
-
-    # Add to loss
-    lat_loss += skewness + kurtosis
-
-    return lat_loss
-
-
-def compute_class_loss(latcls):
-    """ Class loss """
-    k = latcls.size(1)  # Get number of classes
-
-    # 1. Find 999 lowest class values, i.e. leave out the highest class value
-    topk_values, _ = torch.topk(latcls, k=k-1, dim=1, largest=False)
-    # 2. Multiply by 50 -> any value over or equal to 0.02 will be higher or equal to 1
-    topk_values *= 50
-    # 3. Square -> any value below 1 will decrease, any value above 1 (prev min 0.02) will increase
-    # 4. Take the mean
-    return topk_values.square().mean()
-
-
-def compute_sim_loss_txt_img(text_embed, images_embed):
-    """ Similarity loss comparing embedded text prompt to embedded images
-        Needs to be negative because we want to increase similarity
-    """
-    sim = torch.cosine_similarity(text_embed, images_embed, dim=-1)
-    return -sim.mean()
 
 
 class Latents(torch.nn.Module):
@@ -245,7 +150,9 @@ class CLIPGAN:
         self.clip_model, clip_transform = clip.load(clip_name_or_path, device=self.device)
 
         # Load generator model
-        self.generator = BigGAN.from_pretrained(g_name_or_path).to(self.device).eval()
+        self.generator = BigGAN.from_pretrained(g_name_or_path,
+                                                cache_dir='./models/'
+                                                ).to(self.device).eval()
 
         self.normalize = Normalize(  # ImageNet means and std by channel for CLIP input pre-processing
             (0.48145466, 0.4578275, 0.40821073),
@@ -268,29 +175,26 @@ class CLIPGAN:
 
     def run(self):
         for step in range(1, self.steps+1):
-            self.optimize(step)
+            best_img = self.optimize(step)
+            self.save_images(best_img, step)
 
     def optimize(self, step):
-        # # Get latent variables and class labels
-        # latvecs, latcls = self.latents()
-        #
-        # # Generate BigGAN samples (truncation 1)
-        # with torch.no_grad():
-        #     gen_imgs = self.generator(latvecs, latcls, 1)
-
-        gen_imgs = self.generator(*self.latents(), 1)
-
-        # Data augmentation: random cuts
-        clip_input = data_augmentation(gen_imgs, num_cuts=128)
-        clip_input = self.normalize((clip_input + 1) / 2)  # Normalize
-        gen_imgs_embed = self.clip_model.encode_image(clip_input)  # CLIP image encoding
-
         # Get latent variables and class labels
         latvecs, latcls = self.latents()
 
-        lat_loss = compute_latent_loss(latvecs, threshold=1)  # First loss term: latent loss
-        class_loss = compute_class_loss(latcls)  # Second loss term: class loss
-        sim_loss_txt_img = compute_sim_loss_txt_img(self.text_embed, gen_imgs_embed)  # Third loss term: similarity loss
+        # Generate BigGAN samples (truncation 1)
+        with torch.no_grad():
+            gen_imgs = self.generator(latvecs, latcls, 1)
+
+        # Data augmentation: random cuts
+        clip_input = self.data_augmentation(gen_imgs, num_cuts=128)
+        clip_input = self.normalize((clip_input + 1) / 2)  # Normalize
+        gen_imgs_embed = self.clip_model.encode_image(clip_input)  # CLIP image encoding
+
+        lat_loss = self.compute_latent_loss(latvecs, threshold=1)
+        class_loss = self.compute_class_loss(latcls)
+        sim_loss_txt_img = self.compute_sim_loss_txt_img(self.text_embed,
+                                                         gen_imgs_embed)
 
         # Losses to be minimized
         # 1. Latent loss
@@ -304,12 +208,99 @@ class CLIPGAN:
 
         # Pick index of best image (lowest class loss)
         best = torch.topk(class_loss, k=1, largest=False)[1]
-        self.save_imgs(gen_imgs[best:best+1], step)
+        return gen_imgs[best:best+1]
 
-    def save_imgs(self, image, step):
+    def data_augmentation(self, images, num_cuts=32):
+        """ Increase the number of images by cutting out random subimages """
+        _, _, width, height = images.size()  # Get image dimensions
+        assert width == height, "Images must be squares"
+
+        cuts = []
+        for _ in range(num_cuts):
+            # Determine a smaller size to cut images out of the generated samples
+            # 1. Draw one sample from a Gaussian (centered at 0.8 with std dev 0.3)
+            # 2. Limit the value to the range [0.5, 0.95]
+            # 3. Multiply original size with smaller factor
+            # 4. Convert to integer
+            size = int(width * torch.zeros(1,).normal_(.8, .3).clip(.5, .95))
+
+            # Get random offsets from the edge of the image
+            offsetx = torch.randint(0, width - size, ())
+            offsety = torch.randint(0, width - size, ())
+
+            # Cut out images from generated samples
+            # Dimensions are [Batch, Channels, Width, Height]
+            # Here only width and height are touched
+            cut = images[:, :, offsetx:offsetx + size, offsety:offsety + size]
+
+            # Scale random cuts to CLIP input size
+            cut = torch.nn.functional.interpolate(cut, (224, 224), mode='nearest')
+
+            cuts.append(cut)  # Collect cuts in list
+        return torch.cat(cuts, dim=0)  # Create tensor from list
+
+    def compute_latent_loss(self, latvecs, threshold=1):
+        """ Latent vector loss
+            Force distribution of values in latent vectors to be as close as
+            possible to a standard Gaussian
+        """
+        threshold = torch.tensor(threshold, device=latvecs.device)
+
+        # First and second standardized moments (mean and standard deviation)
+        mean = latvecs.mean().abs()  # Absolute mean of latents
+        # Mean of absolute difference of lat vec’s stdev to 1 (normal Gaussian)
+        std = torch.abs(1 - latvecs.std(dim=1)).mean()
+        max = torch.max(latvecs.square().mean(),  # Mean of squared latent vecs
+                        threshold)  # Latent threshold (default: 1)
+
+        lat_loss = mean + std + 4 * max
+
+        # Score normalization for calculation of skewness and kurtosis
+        # In batch but for each latent vector (each row) separately
+        vec_means = latvecs.mean(dim=1, keepdim=True)  # Compute means
+        vec_diffs = latvecs - vec_means  # Compute deviations from means
+        vec_vars = vec_diffs.square().mean(dim=1, keepdim=True)  # Variances
+        vec_stds = vec_vars.sqrt()  # Standard deviations
+        vec_scores = vec_diffs / vec_stds  # Normalized scores
+
+        # Third and fourth standardized moments
+        # Compute skewness and kurtosis of values in latent vector
+        skewness = vec_scores.pow(3.0).mean(dim=1)
+        kurtosis = vec_scores.pow(4.0).mean(dim=1) - 3.0
+
+        # Absolute deviations from normal Gaussian skewness (0) and kurtosis (3)
+        # Compute average over all vectors (rows)
+        skewness = skewness.abs().mean()
+        kurtosis = kurtosis.abs().mean()
+
+        # Add to loss
+        lat_loss += skewness + kurtosis
+
+        return lat_loss
+
+    def compute_class_loss(self, latcls):
+        """ Class loss """
+        k = latcls.size(1)  # Get number of classes
+
+        # 1. Find 999 lowest class values, i.e. leave out the highest class value
+        topk_values, _ = torch.topk(latcls, k=k-1, dim=1, largest=False)
+        # 2. Multiply by 50 -> any value over or equal to 0.02 will be higher or equal to 1
+        topk_values *= 50
+        # 3. Square -> any value below 1 will decrease, any value above 1 (prev min 0.02) will increase
+        # 4. Take the mean
+        return topk_values.square().mean()
+
+    def compute_sim_loss_txt_img(self, text_embed, images_embed):
+        """ Similarity loss comparing embedded text prompt to embedded images
+            Needs to be negative because we want to increase similarity
+        """
+        sim = torch.cosine_similarity(text_embed, images_embed, dim=-1)
+        return -sim.mean()
+
+    def save_images(self, image, step):
         pad = len(str(self.steps))  # Number of digits for padding
-        filename = f"frame_{step:0{pad}}.png"  # Pad filename with zeros
-        save_image(image, self.save_path/filename,
+        filename = f"frame_{step:0{pad}}.png"  # Prefix filename with zeros
+        save_image(image, self.save_path/filename,  # torchvision method
                    normalize=True, range=(-1., +1.))
 
 
